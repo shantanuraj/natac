67ee839fd4b665d2049b1db1836da676c10a1401
Shantanu Raj <s@sraj.me>
2020-05-15 23:01:58 +0530

    chore(layout): Update footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,7 @@
 export function Footer () {
   return (
     <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-      </a>
+      © {new Date().getFullYear()}<a href="https://sraj.me" target="_blank" rel="noopener noreferrer">Shantanu Raj</a>
     </footer>
   )
 }
